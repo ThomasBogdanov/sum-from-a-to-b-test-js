eeeb41954d91c1090c07d39a829bef97e8c1c1d8
Thomas Bogdanov <thomasbogdanov1@gmail.com>
2019-08-23 16:27:22 +0000

    index code

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  if (fromN === toN) {
+    return toN
+  }
+
+  return fromN + sum(fromN + 1, toN)
+
 }
 
-module.exports = sum;
+//module.exports = sum;
+console.log(sum(3, 7));
+
+/*
+
+sum (3, 7) = 3 + 4 + 5 + 6 + 7
+sum (4, 7) = 4 + 5 + 6 + 7
+sum (5, 7) = 5 + 6 + 7
+sum (6, 7) = 6 + 7
+sum (7, 7) = 7
+
+sum (n, N) = N
+sum (n-1, N) = (n-1) + N
+sum ()
+
+*/
